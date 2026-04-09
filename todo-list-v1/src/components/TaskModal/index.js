d1b8513a844db039545419249abe41e97523bb21
nikidsy <dianaoliveirandrade@gmail.com>
2026-04-09 10:16:30 -0300

    feat: add componens TaskModal

---
+++ todo-list-v1/src/components/TaskModal/index.js
@@ -0,0 +1,54 @@
+import {Modal, View, Text, TextInput, TouchableOpacity} from "react-naive";
+import styles from "./style.js";
+import {useEffect, useState} from "react";
+
+export default function TaskModal({visible, onClose, onSave, taskToEdit}) {
+    cosnt [form, setForm] = useState({
+        title: "",
+        description: "",
+        startDate: "",
+        endDate: "",
+        priority: 'Média'
+    });
+
+    useEffect(() => {
+        if (taskToEdit) setForm(taskToEdit);
+        else setForm({
+            title: "",
+            description: "",
+            startDate: "",
+            endDate: "",
+            priority: 'Média'
+        });
+    }, [taskToEdit, visible]);
+
+    return (
+        <Modal visible={visible} animation="slide">
+            <View style={styles.modalBody}>
+                <Text style={styles.label}>
+                    {taskToEdit ? 'Editar Tarefa' : 'Nova Tarefa'}
+                </Text>
+                <TextInput style={styles.input} placeholder='Título' value={form.title} onChangeText={v => setForm({
+                    ...form, title: v})}/>
+                <TextInput style={styles.input} placeholder='Descrição' value={form.description} onChangeText={v => setForm({
+                    ...form, description: v})}/>
+                <TextInput style={styles.input} placeholder='Data de Início: DD/MM' value={form.startDate} onChangeText={v => setForm({
+                    ...form, startDate: v})}/>
+                <TextInput style={styles.input} placeholder='Data de Fim: DD/MM' value={form.endDate} onChangeText={v => setForm({
+                    ...form, endDate: v})}/>
+                <TextInput style={styles.input} placeholder='Prioridade' value={form.priority} onChangeText={v => setForm({
+                    ...form, priority: v})}/>
+                <TouchableOpacity style={styles.btnSave} onPress={() => onSave(form)}>
+                    <Text style={styles.btnText}>
+                        Salvat Tarefa
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={onClose}>
+                    <Text style={styles.btnCancelar}>
+                        Cancelar
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </Modal>
+    );
+}
